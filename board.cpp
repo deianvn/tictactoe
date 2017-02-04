27685a37e545066aa096f469807c08c40be9fa3c
Deyan Rizov <deianvn@gmail.com>
2017-02-04 18:07:02 +0200

    added build directory to gitignore

--- board.cpp
+++ board.cpp
@@ -23,7 +23,7 @@ namespace tictactoe
         {
             return false;
         }
-        
+
         return at(x, y) == ' ';
     }
 
@@ -47,12 +47,12 @@ namespace tictactoe
     std::vector<position> board::available_positions() const
     {
         std::vector<position> v;
-        
+
         for (int i = 0; i < SIZE; i++)
             for (int j = 0; j < SIZE; j++)
                 if (at(j, i) == ' ')
                     v.push_back(std::make_pair(j, i));
-        
+
         return v;
     }
 
@@ -64,34 +64,34 @@ namespace tictactoe
     bool board::wins() const
     {
         char s1, s2, s3;
-        
+
         for (int i = 0; i < SIZE; i++)
         {
             s1 = at(0, i);
             s2 = at(1, i);
             s3 = at(2, i);
-            
+
             if (s1 != ' ' && s1 == s2 && s2 == s3) return true;
-            
+
             s1 = at(i, 0);
             s2 = at(i, 1);
             s3 = at(i, 2);
-            
+
             if (s1 != ' ' && s1 == s2 && s2 == s3) return true;
         }
-        
+
         s1 = at(1, 1);
-        
+
         if (s1 == ' ') return false;
-        
+
         for (int i = 0; i < SIZE; i+= 2)
         {
             s2 = at(0, i);
             s3 = at(2, 2 - i);
-            
+
             if (s1 != ' ' && s1 == s2 && s2 == s3) return true;
         }
-        
+
         return false;
     }
 
@@ -102,7 +102,7 @@ namespace tictactoe
             throw std::exception {};
         }
     }
-    
+
     int board::convert_2d_to_1d_coordinates(int x, int y) const
     {
         return y * SIZE + x;
